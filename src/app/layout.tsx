e5910987ab7cfb27d2298218a5ea4c0953c670b3
a <a@pop-os.localdomain>
2023-10-17 14:50:01 +0200

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,23 @@
-import FlexCol from "@/components/FlexCol"
-import "./globals.css"
-import { Inter } from "next/font/google"
+import "./globals.css";
+import { Inter } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
-})
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`bg-fill ${inter.className}`}>
-        <FlexCol className="justify-center">
-          <div className="w-full h-2/3">
-            {children}
-          </div>
-        </FlexCol>
+        <div className="flex flex-col justify-center w-full h-screen">
+          <div className="w-full h-2/3">{children}</div>
+        </div>
       </body>
     </html>
-  )
+  );
 }
--- src/components/Week/WeekContainer.tsx
+++ src/components/Week/WeekContainer.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
-
 type WeekProps = {
   children?: React.ReactNode;
 };
-
 
 const WeekContainer: React.FC<WeekProps> = ({ children }) => {
   return (
