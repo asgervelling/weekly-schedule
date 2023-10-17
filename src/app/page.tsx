9aca9aa9b28258e9c99ec9e75e4e5cedde4f56a1
a <a@pop-os.localdomain>
2023-10-17 15:13:25 +0200

    Removed Schedule component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Button from "@/components/Button";
-import { randomWeek } from "@/mockdata/event_data";
 import { CalendarEvent } from "../../types";
-import Schedule from "@/components/Schedule";
 import { parseTs } from "@/calendar/timestamps";
+import Week from "@/components/Week";
 
 export default function Home() {
   const [days, setDays] = useState<CalendarEvent[][] | null>(null);
@@ -33,7 +32,7 @@ export default function Home() {
           <Button onClick={updateDays}>Click me!</Button>
         </div>
         <div className="flex flex-col items-center justify-center w-3/5">
-          <Schedule days={days} />
+          <Week days={days} />
         </div>
         <div className="flex flex-col items-start justify-end w-1/5">
           <Button onClick={() => console.log("Hi")}>Button 2</Button>
--- src/components/Week/Week.tsx
+++ src/components/Week/Week.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import Day from '../Day';
-import { CalendarEvent } from '../../../types';
-import WeekContainer from './WeekContainer';
-
+import React from "react";
+import Day from "../Day";
+import { CalendarEvent } from "../../../types";
 
 type WeekProps = {
-  days: CalendarEvent[][];
+  days: CalendarEvent[][] | null;
 };
-
 
 /**
  * Seven days of the week.
- * 
+ *
  * @param days - Seven lists of events.
  */
 const Week: React.FC<WeekProps> = ({ days }) => {
+  const renderDays = () => {
+    return days?.map((day, i) => <Day key={i} events={day} />);
+  };
+
+  const renderFallback = () => {
+    return (
+      <div className="flex items-center justify-center w-full h-full">
+        Loading...
+      </div>
+    );
+  };
+
   return (
-    <WeekContainer>
-      {days.map((day, i) => (
-        <Day key={i} events={day} />
-      ))}
-    </WeekContainer>
+    <div className="flex w-full h-full overflow-y-auto border border-black rounded">
+      {days !== null ? renderDays() : renderFallback()}
+    </div>
   );
 };
 
