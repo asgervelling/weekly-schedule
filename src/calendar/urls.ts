adfc0723ede787a41f35e7da0d0de1b60645cbcd
a <a@pop-os.localdomain>
2023-10-20 17:50:13 +0200

    Simplified urls.ts

--- src/calendar/urls.ts
+++ src/calendar/urls.ts
@@ -3,7 +3,6 @@ import { Week } from "../../types";
 import { emptyWeek } from "./events";
 import * as E from "fp-ts/Either";
 import * as O from "fp-ts/Option";
-import { Either } from "fp-ts/lib/Either";
 
 /**
  * Try to parse a string as JSON.
@@ -11,10 +10,14 @@ import { Either } from "fp-ts/lib/Either";
  * @param s A string that may or may not be JSON.
  * @returns Either an error or the parsed JSON.
  */
-const parseJson: (s: string) => Either<Error, Week> = E.tryCatchK(
-  JSON.parse,
-  E.toError
-);
+const parseJson: (s: O.Option<string>) => E.Either<Error, Week> = (json) =>
+  pipe(
+    json,
+    O.fold(
+      () => E.right(emptyWeek()), // If the option is None
+      (jsonStr) => E.tryCatch(() => JSON.parse(jsonStr), E.toError)
+    )
+  );
 
 /**
  * Get the week parameter from the query string.
@@ -25,13 +28,12 @@ const parseJson: (s: string) => Either<Error, Week> = E.tryCatchK(
  */
 const getWeekJson = (searchParams: {
   week: string | undefined;
-}): O.Option<string> => {
-  return pipe(
+}): O.Option<string> =>
+  pipe(
     searchParams.week,
     O.fromNullable,
     O.chain(O.fromPredicate((s) => s !== ""))
   );
-};
 
 /**
  * If there are search params, try to parse them as a Week.
@@ -39,19 +41,13 @@ const getWeekJson = (searchParams: {
  */
 export const getInitialWeek = (searchParams: {
   week: string | undefined;
-}): Week => {
-  return pipe(
+}): Week =>
+  pipe(
     searchParams,
     getWeekJson,
-    O.match(emptyWeek, (json) =>
-      pipe(
-        json,
-        parseJson,
-        E.getOrElse((error) => {
-          console.error(`Error parsing week: ${error.message}`);
-          return emptyWeek();
-        })
-      )
-    )
+    parseJson,
+    E.getOrElse((error) => {
+      console.error(`Error parsing week: ${error.message}`);
+      return emptyWeek();
+    })
   );
-};
